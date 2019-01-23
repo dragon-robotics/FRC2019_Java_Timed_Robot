61e6f2b2dbc0b84d235db1b181717bf3f4d01a0a
Doug Liu <dougdliu@gmail.com>
2019-01-23 00:13:17 -0700

    1. Refactored code and cleaned up comments 2. Changed Front/Rear motor grouping to Left/Right motor grouping 3. Modified joystick so left a-stick is Y and right a-stick is X. 3. Added more joystick buttons 4. Added pneumatics testing code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,19 +7,24 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMTalonSRX;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.GenericHID.Hand;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Joystick;                  // A WPI library used for joystick control
+import edu.wpi.first.wpilibj.PWMVictorSPX;              // A WPI library used for PWM Victor Motor Contorller
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.TimedRobot;                // A WPI library used for Timed Robot base class
+import edu.wpi.first.wpilibj.Timer;                     // A WPI library used for timing robot operations
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;    // A WPI library used for joystick buttons mapping
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;   // A WPI library used for controlling differential drive
+import edu.wpi.first.wpilibj.SpeedControllerGroup;      // A WPI library used for grouping motors for synchronized controls
+
+/* Pneumatics Control */
+import edu.wpi.first.wpilibj.Compressor;                // A WPI library used for controlling and obtaining the status of the compressor
+import edu.wpi.first.wpilibj.DoubleSolenoid;            // A WPI library used for controlling and obtaining the status of the double solenoid
 
 /* For TalonSRX operation */
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;       // A CTRE library used for control mode selection in the CTRE library
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;  // A CTRE library used for TalonSRX control using CAN protocol
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -30,37 +35,47 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
  */
 public class Robot extends TimedRobot {
 
-  /* Front Motors */
-  private final PWMVictorSPX m_frontRight = new PWMVictorSPX(4);                        // Front Right Motor using PWM Victor
-  private final PWMVictorSPX m_frontLeft = new PWMVictorSPX(3);                         // Front Left Motor using PWM Victor
-  SpeedControllerGroup m_front = new SpeedControllerGroup(m_frontRight, m_frontLeft);   // Front Right + Front Left synchronized control
+  /* Left Motors */
+  private final PWMVictorSPX m_leftFront = new PWMVictorSPX(2);                         // Left Front Motor using PWM Victor
+  private final PWMVictorSPX m_leftRear = new PWMVictorSPX(0);                          // Left Rear Motor using PWM Victor
+  SpeedControllerGroup m_left = new SpeedControllerGroup(m_leftFront, m_leftRear);      // Left Front + Left Rear synchronized control
   
-  /* Rear Motors */
-  private final PWMVictorSPX m_rearRight = new PWMVictorSPX(2);                         // Rear Right Motor using PWM Victor
-  private final PWMVictorSPX m_rearLeft = new PWMVictorSPX(1);                          // Rear Left Motor using PWM Victor
-  SpeedControllerGroup m_rear = new SpeedControllerGroup(m_rearRight, m_rearLeft);      // Rear Right + Rear Left synchronized control
+  /* Right Motors */
+  private final PWMVictorSPX m_rightFront = new PWMVictorSPX(3);                        // Right Front Motor using PWM Victor
+  private final PWMVictorSPX m_rightRear = new PWMVictorSPX(1);                         // Right Rear Motor using PWM Victor
+  SpeedControllerGroup m_right = new SpeedControllerGroup(m_rightFront, m_rightRear);   // Right Front + Right Rear synchronized control
   
   /* Robot Drive Combination */
   private final DifferentialDrive m_robotDrive
-    = new DifferentialDrive(m_front, m_rear);
+    = new DifferentialDrive(m_left, m_right);
   
   /* Landing Gear TalonSRX */
-  private final WPI_TalonSRX m_landingGearLeft = new WPI_TalonSRX(1);     //  Left landingGear
-  private final WPI_TalonSRX m_landingGearRight = new WPI_TalonSRX(2);
+  private final WPI_TalonSRX m_landingGearLeft = new WPI_TalonSRX(1);     // Left Landing Gear
+  private final WPI_TalonSRX m_landingGearRight = new WPI_TalonSRX(2);    // Right Landing Gear
   SpeedControllerGroup m_landingGear 
-    = new SpeedControllerGroup(m_landingGearLeft, m_landingGearRight);
+    = new SpeedControllerGroup(m_landingGearLeft, m_landingGearRight);    // Landing Gear Group
 
   /* Robot Driver - AKA Driver 1 */
-  private final Joystick j_stick_driver_1 = new Joystick(1);
+  private final Joystick j_stick_driver = new Joystick(1);  // Drive joystick initialization
 
   /* Robot Controller - AKA Driver 2 */
-  private final Joystick j_stick_driver_2 = new Joystick(0);
-  private final JoystickButton j_stick_driver_2_LB = new JoystickButton(j_stick_driver_2, 5);
-  private final JoystickButton j_stick_driver_2_RB = new JoystickButton(j_stick_driver_2, 6);
-      
+  private final Joystick j_stick_control = new Joystick(0);  // Control joystick initialization
+  private final JoystickButton j_stick_control_LB = new JoystickButton(j_stick_control, 5); // Left button
+  private final JoystickButton j_stick_control_RB = new JoystickButton(j_stick_control, 6); // Right button 
+  private final JoystickButton j_stick_control_X = new JoystickButton(j_stick_control, 3);  // X button
+  private final JoystickButton j_stick_control_Y = new JoystickButton(j_stick_control, 4);  // Y button
+  private final JoystickButton j_stick_control_A = new JoystickButton(j_stick_control, 1);  // A button
+  private final JoystickButton j_stick_control_B = new JoystickButton(j_stick_control, 2);  // B button
+  
   /* Timers */
   private final Timer m_timer = new Timer();
   private final Timer landingGear_timer = new Timer();
+
+  /* Compressor */
+  private final Compressor compressor = new Compressor(0);
+  
+  /* Double Solenoid */
+  private final DoubleSolenoid doubleSolenoid = new DoubleSolenoid(1,2);
 
   /**
    * This function is run when the robot is first started up and should be
@@ -116,21 +131,23 @@ public class Robot extends TimedRobot {
       How the code works / is implemented:
         - Because the teleopPeriodic() function is repeatedly called every 20 milliseconds, we can consider all of the code inside of a function
           as if it's ran inside of a loop. Therefore, when we write the code, we want to be wary of the fact that there might be code that don't 
-          need to be used until it's needed. In this case, the best way to think about this loop is a  do(action)...until(condition) loop. The 
+          need to be used until it's needed. In this case, the best way to think about this loop is a do(action)...until(condition) loop. The 
           reason for this is because for the first case, we want to keep the landing gear motor running for some amount of time before stopping. 
           Therefore our action="keep running the landing gear motor at full power based on which button is pressed" and our condition="X amount 
-          of seconds". To keep track of time, I have the timer reset and immediately start after resetting. After checking whether left or right 
-          button is pressed, we set the motor power and direction accordingly. Our end condition is checking whether or not we are equal to or 
-          greater than the amount of time we want the landing gear motors to run for. For the second case, because we are continually holding the
-          button, we want the landing gears motor to run indefinitely. To do this, we have to reset and start the timer every single time a left
-          or right button is held because we never want our timer to be above 0.3 seconds. One last thing is we also want to make sure only one
-          button is pressed and when both button is pressed, we retain to prevent unknown behaviors in the code. This is done through the 
-          "^" (XOR or exclusive or) operation when we check exclusively whether which button was pressed.
+          of seconds". To keep track of time, I have the timer reset and immediately start after resetting after the left or right button is
+          pressed. After checking whether left or right button is pressed, we set the motor power and direction accordingly. Our end condition 
+          is checking whether or not we are equal to or greater than the amount of time we want the landing gear motors to run for. 
+          
+          For the second case, because we are continually holding the button, we want the landing gears motor to run indefinitely. To do this,
+          we have to reset and start the timer every single time a left or right button is held because we never want our timer to be above 0.3
+          seconds. One last thing is we also want to make sure only one button is pressed and when both button is pressed, we retain to prevent
+          unknown behaviors in the code. This is done through the "^" (XOR or exclusive or) operation when we check exclusively whether which 
+          button was pressed.
     */
 
 
-    boolean LB_pressed = j_stick_driver_2_LB.get();   // Check whether the LB is pressed or not. Returns a boolean value (True/False)
-    boolean RB_pressed = j_stick_driver_2_RB.get();   // Check whether the RB is pressed or not. Returns a boolean value (True/False) 
+    boolean LB_pressed = j_stick_control_LB.get();   // Check whether the LB is pressed or not. Returns a boolean value (True/False)
+    boolean RB_pressed = j_stick_control_RB.get();   // Check whether the RB is pressed or not. Returns a boolean value (True/False) 
 
     // Check if either button is exclusively pressed, (^ is the XOR operator) //
     if(LB_pressed ^ RB_pressed){
@@ -163,9 +180,9 @@ public class Robot extends TimedRobot {
     }
     
     /* Drive the robot with joystick control. Left analog stick controls up/down. Right analog stick controls left/right */
-    double leftJoyY = -j_stick_driver_1.getY(Hand.kLeft);   // This is reversed because of arcade drive implementation
-    double rightJoyX = j_stick_driver_1.getX(Hand.kRight);  // Grab right analog stick X value
-    m_robotDrive.arcadeDrive(leftJoyY, rightJoyX);          // Drive the robot using arcade drive
+    double leftJoyY = j_stick_driver.getRawAxis(1);    // Grab left analog stick's X value
+    double rightJoyX = j_stick_driver.getRawAxis(6);   // Grab right analog stick's Y value 
+    m_robotDrive.arcadeDrive(leftJoyY, rightJoyX);       // Drive the robot using arcade drive
   }
 
   /**
@@ -173,10 +190,37 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
-    double x = j_stick_driver_1.getX();
-    double y = j_stick_driver_1.getY();
 
-    System.out.println("X: "+x+", Y: "+y);
-    m_robotDrive.arcadeDrive(j_stick_driver_1.getY(), j_stick_driver_1.getX());
+    // This is used for testing //
+
+    // Testing Joystick Motor Control //
+    // double x = j_stick_driver.getX();
+    // double y = j_stick_driver.getY();
+    // m_robotDrive.arcadeDrive(j_stick_driver.getY(), j_stick_driver.getX());
+
+    // Testing Pneumatics and Double Solenoid Code //
+    boolean X_pressed = j_stick_control_X.get();  // Get X pressed value
+    boolean Y_pressed = j_stick_control_Y.get();  // Get Y pressed value
+    boolean A_pressed = j_stick_control_A.get();  // Get A pressed value
+    boolean B_pressed = j_stick_control_B.get();  // Get B pressed value
+
+    if(X_pressed){
+      // Forward Pneumatics //
+      doubleSolenoid.set(Value.kForward);
+    }
+    else if(Y_pressed){
+      // Reverse Pneumatics //
+      doubleSolenoid.set(Value.kReverse);
+    }
+    else if(A_pressed){
+      // Off Pneumatics //
+      doubleSolenoid.set(Value.kOff);
+    }
+    else{
+      // Toggle Compressor //
+      boolean compressor_enabled = !compressor.enabled();
+      compressor.setClosedLoopControl(compressor_enabled);
+    } 
+
   }
 }
